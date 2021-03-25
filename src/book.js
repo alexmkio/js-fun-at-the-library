10f97193216de0c02ae9a2a40dbf78202e5d1b5b
Alex Kio <alexandermartelkio@gmail.com>
2021-03-25 08:50:19 -0400

    Add functions - 2 versions of buildMainCharacter

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "modified " + title;
 }
+createTitle("Book Title 1")
+
+function BuildMainCharacter(character) {
+   this.character = character;
+}
+function BuildMainCharacter(name, birthPlace, age) {
+  this.characterName = name;
+  this.characterBirthPlace = birthPlace;
+  this.characterAge = age;
+}
+var character = new BuildMainCharacter("Prince Jones Jr.", "Washington D.C.", 25);
+// character
+// https://codeburst.io/various-ways-to-create-javascript-object-9563c6887a47
+// https://www.programiz.com/javascript/constructor-function
+
+function saveReview() {
+
+}
+
+
+function calculatePageCount() {
+
+}
+
+
+function writeBook() {
+
+}
+
+
+function editBook() {
+
+}
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
