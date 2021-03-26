6616d8c6b5b5fd52e0ce6481c83a8d33c8e8a773
Alex Kio <alexandermartelkio@gmail.com>
2021-03-25 21:55:12 -0400

    Add code for reviews

--- src/book.js
+++ src/book.js
@@ -16,13 +16,25 @@ var character = new buildMainCharacter("Prince Jones Jr.", "Washington D.C.", 25
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects#using_a_constructor_function
 // looks good to me so why doesn't it pass? (new)
 
-var reviews = []
-function saveReview() {
-
+// below works
+// var reviews = new Set(['good', 'bad']);
+// function saveReview(review) {
+//   if (reviews.has(review)) {
+//     console.log('has review')
+//   } else {
+//     console.log('doesnt have that one')
+//   }
+// }
+var reviews = new Set(['good', 'bad']);
+function saveReview(review) {
+  if (reviews.has(review)) {
+    return
+  } else {
+    reviews.push(review)
+  }
 }
-// https://stackoverflow.com/questions/7376598/in-javascript-how-do-i-check-if-an-array-has-duplicate-values
-// https://www.codegrepper.com/code-examples/javascript/add+only+unique+values+to+array+javascript+using+spread+operator
-// set looks real cool: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+// saveReview('')
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 
 function calculatePageCount() {
 
