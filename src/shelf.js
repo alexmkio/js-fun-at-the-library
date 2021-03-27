27e6bf415cd9a7d19c6a9540160f49d18e2bebed
Alex Kio <alexandermartelkio@gmail.com>
2021-03-27 18:23:26 -0400

    Commit initial completion of shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -10,14 +10,33 @@ function unshelfBook(title, shelf) {
   for (var i = 0; i < shelf.length; i++) {
     if (title === shelf[i].title) {
       shelf.splice(i, 1)
-    } else {
     }
   }
+}
+
+function listTitles(shelf) {
+  var shelfTitles = []
+  for (var i = 0; i < shelf.length; i++) {
+    shelfTitles.push(shelf[i].title)
+  }
+    return shelfTitles.join(', ')
+}
+
+function searchShelf(shelf, title) {
+  var isOnShelf = false
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      isOnShelf = true
+    } else {
+      isOnShelf = false
+    }
+  }
+  return isOnShelf
 }
 
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
