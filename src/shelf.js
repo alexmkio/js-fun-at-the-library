7fb5ee4840773d8fe1d5165cf6a8a833ac42e3c9
Alex Kio <alexandermartelkio@gmail.com>
2021-03-27 13:51:15 -0400

    Refactor shelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,11 +1,3 @@
-// function shelfBook(book, shelf) {
-//   if (shelf.length < 3) {
-//     return shelf.unshift(book)
-//   } else {
-//     return shelf
-//   }
-// }
-
 function shelfBook(book, shelf) {
   if (shelf.length >= 3) {
     return shelf
@@ -13,15 +5,6 @@ function shelfBook(book, shelf) {
     return shelf.unshift(book)
   }
 }
-
-// why doesn't the below work?
-// function shelfBook(book, shelf) {
-//   if (shelf.length > 3) {
-//     return shelf
-//   } else {
-//     return shelf.unshift(book)
-//   }
-// }
 
 module.exports = {
   shelfBook,
