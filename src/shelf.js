487fd3524703c0fd29f24dd2dd1a24c3721f69db
Alex Kio <alexandermartelkio@gmail.com>
2021-03-27 13:50:47 -0400

    Add all my questions and solutions regarding shelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,30 @@
+// function shelfBook(book, shelf) {
+//   if (shelf.length < 3) {
+//     return shelf.unshift(book)
+//   } else {
+//     return shelf
+//   }
+// }
+
+function shelfBook(book, shelf) {
+  if (shelf.length >= 3) {
+    return shelf
+  } else {
+    return shelf.unshift(book)
+  }
+}
+
+// why doesn't the below work?
+// function shelfBook(book, shelf) {
+//   if (shelf.length > 3) {
+//     return shelf
+//   } else {
+//     return shelf.unshift(book)
+//   }
+// }
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
