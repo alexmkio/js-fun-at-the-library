d77902b51d1a11036c72046c6875e87708720900
Alex Kio <alexandermartelkio@gmail.com>
2021-03-27 16:29:17 -0400

    Update unshelfBook with comments

--- src/shelf.js
+++ src/shelf.js
@@ -6,9 +6,32 @@ function shelfBook(book, shelf) {
   }
 }
 
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    // if (shelf.includes(title)) {
+    if (title === shelf[i].title) {
+      shelf.splice(i, 1)
+      // console.log(shelf.indexOf(book))
+      // var position = shelf.indexOf(book)
+      // shelf.splice(position, 1)
+    } else {
+    }
+  }
+}
+
+  // for (var i = 0; i = shelf.length; i++) {
+  //   console.log(shelf[i]); }
+  // console.log(shelf.includes('Hyperion'))
+  // if (shelf.book[title] = title {
+  //   return shelf
+  // } else {
+  //   return shelf.unshift(book)
+  // }
+// }
+
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
